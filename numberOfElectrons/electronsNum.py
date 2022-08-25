95b925f7ed1c714a0566f85a1aecc574ce551556
vikoluna <victor.lunam@alumno.buap.mx>
2022-08-24 19:01:51 -0500

    Corrections

--- numberOfElectrons/electronsNum.py
+++ numberOfElectrons/electronsNum.py
@@ -1,15 +1,20 @@
 # Created by. Viko Luna
 # Calcular el numero de electrones en n número de moles a partir de los datos de la tabla periodica
 import pandas as pd
+
 data = pd.read_csv('elements.csv')
 print(data)
 
-elementos = data.drop(['Period':], axis=1)
+elementos = data.drop(columns=['Period', 'Group', 'Phase', 'Radioactive', 'Natural', 'Metal', 'Nonmetal',
+                               'Metalloid', 'Type', 'Nonmetal', 'Metalloid', 'Type', 'AtomicRadius',
+                               'Electronegativity', 'FirstIonization', 'Density', 'MeltingPoint', 'BoilingPoint',
+                               'NumberOfIsotopes', 'Discoverer', 'Year', 'SpecificHeat', 'NumberofShells',
+                               'NumberofValence'])
+
 print(elementos)
 
-
-
 print("Introduzca el número atómico del elemento: ")
+#
 # numAtom = input()
 
 print("Los datos del elemento ingresado son: ")
@@ -21,4 +26,5 @@ print("Introduzca la masa de la sustancia en gramos: ")
 # Numero de moles
 # Masa Atomica para calcular el numero de moles
 # masaAtom = data[]
+
 # numMols = masa/
